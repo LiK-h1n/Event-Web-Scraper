6b28615d1d358625688af8bbf506c4a049bc5e9a
LiK-h1n <likh1n709@gmail.com>
2023-07-10 08:27:40 -0700

    Clears events that got over

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from os import getenv
 from email.message import EmailMessage
 
 URL = "https://www.songkick.com/metro-areas/29403-india-bangalore"
+CSV_PATH = "data.csv"
 
 
 def scrape(url):
@@ -86,7 +87,7 @@ def store(row):
         row : list
             The row that has to be stored
     """
-    with open("data.csv", "a", encoding="utf-8") as file:
+    with open(CSV_PATH, "a", encoding="utf-8") as file:
         wrt = writer(file, lineterminator="\n")
         wrt.writerow(row)
 
@@ -100,26 +101,31 @@ def read():
         DataFrame
             DataFrame containing event details
     """
-    return read_csv("data.csv")
+    return read_csv(CSV_PATH)
 
 
 if __name__ == "__main__":
     while True:
         scrapped = scrape(URL)
-        events = extract(scrapped)
-        stored = read()
+        upcoming_events = extract(scrapped)
+        stored_events = read()
+
+        for index, event in enumerate(stored_events.values.tolist()):
+            if event not in upcoming_events:
+                stored_events.drop(index, inplace=True)
+        stored_events.to_csv(CSV_PATH, index=False)
 
         event_stored = False
-        if len(events) != 0:
-            for event in events:
-                if event not in stored.values.tolist():
+        if len(upcoming_events) != 0:
+            for event in upcoming_events:
+                if event not in stored_events.values.tolist():
                     store(event)
                     event_stored = True
 
-            stored = read()
+            stored_events = read()
             if event_stored:
                 message_content = ""
-                for index, row in stored.iterrows():
+                for index, row in stored_events.iterrows():
                     message_content += f"{row['Name']}\n{row['Venue']}, {row['Date']}\n{row['Link']}\n\n"
 
                 send_email(message_content)
